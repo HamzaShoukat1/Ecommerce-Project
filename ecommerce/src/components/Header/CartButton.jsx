f8db6f10377a9eedd482eec55a543e2875ded7fe
hamza <hamza123ali45678@gmail.com>
2025-08-22 01:57:31 +0500

    add cartbutton

--- ecommerce/src/components/Header/CartButton.jsx
+++ ecommerce/src/components/Header/CartButton.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router";
 function CartButton({ cartCount ,MdShoppingCart}) {
+  const cartCount = useSelector((state) => state.Cart.totalQuantity);
+
   return (
     <NavLink to="/cart" className="relative">
       <MdShoppingCart className="text-2xl block" /> 
